d2bea111f763162f9616f94f2b34335ff85cab53
eyyrone <eyyrone@gmail.com>
2023-11-29 22:00:15 +0100

    Edit finetune bart file

--- finetune_model.py
+++ finetune_model.py
@@ -1,44 +1,51 @@
 from datasets import load_dataset
-from transformers import BartForConditionalGeneration, BartTokenizer, Trainer, TrainingArguments
+from transformers import BartTokenizer, BartForConditionalGeneration, Trainer, TrainingArguments
 
 # Load the dataset
-dataset = load_dataset('cnn_dailymail', '3.0.0')
+dataset = load_dataset("cnn_dailymail", '3.0.0')
 
-# Prepare the data
-tokenizer = BartTokenizer.from_pretrained('facebook/bart-base')
+# Load the tokenizer
+tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-cnn')
 
-def tokenize_function(examples):
-    inputs = tokenizer(examples['article'], truncation=True, max_length=512, padding='max_length', return_tensors='pt')
-    outputs = tokenizer(examples['highlights'], truncation=True, max_length=150, padding='max_length', return_tensors='pt')
-    inputs["labels"] = outputs["input_ids"]
-    return inputs
+# Tokenize the dataset
+def preprocess_function(examples):
+    inputs = [doc for doc in examples["article"]] 
+    model_inputs = tokenizer(inputs, max_length=1024, truncation=True)
+    with tokenizer.as_target_tokenizer():
+        labels = tokenizer(examples["highlights"], max_length=142, truncation=True)
+    model_inputs["labels"] = labels["input_ids"]
+    return model_inputs
 
-tokenized_dataset = dataset.map(tokenize_function, batched=True)
+tokenized_datasets = dataset.map(preprocess_function, batched=True)
 
-# Fine-tune the model
-model = BartForConditionalGeneration.from_pretrained('facebook/bart-base')
+# Split the dataset into training and validation sets
+train_dataset = tokenized_datasets["train"]
+eval_dataset = tokenized_datasets["validation"]
 
+# Load the model
+model = BartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn')
+
+# Define the training arguments
 training_args = TrainingArguments(
-    per_device_train_batch_size=4,
-    per_device_eval_batch_size=4,
-    output_dir='./results',
-    num_train_epochs=3,
-    evaluation_strategy='epoch',
-    logging_dir='./logs',
-    logging_steps=100,
-    do_train=True,
-    do_eval=True,
+    output_dir='./results',          # output directory for model checkpoints
+    num_train_epochs=3,              # number of training epochs
+    per_device_train_batch_size=4,   # batch size for training
+    per_device_eval_batch_size=4,    # batch size for evaluation
+    warmup_steps=500,                # number of warmup steps for learning rate scheduler
+    weight_decay=0.01,               # weight decay for optimizer
+    logging_dir='./logs',            # directory for storing logs
+    save_strategy='epoch',           # save strategy to save the model
 )
 
+# Initialize the Trainer
 trainer = Trainer(
     model=model,
     args=training_args,
-    train_dataset=tokenized_dataset["train"],
-    eval_dataset=tokenized_dataset["validation"],
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,
+    tokenizer=tokenizer
 )
 
+# Train the model
 trainer.train()
 
-# Save the fine-tuned model
-model.save_pretrained('./fine_tuned_model')
-tokenizer.save_pretrained('./fine_tuned_model')
