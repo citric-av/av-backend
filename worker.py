6701a2b3c90650ac5b6550590b5858ead3149317
dzeliks <a.bandic@student.fontys.nl>
2024-01-23 22:37:33 +0100

    Updated worker for Docker

--- worker.py
+++ worker.py
@@ -1,11 +1,12 @@
 import redis
 from rq import Worker, Queue, Connection
+import os
 
 # Queues we want our workers to listen to
 listen = ['default']
 
 # URL for our Redis server
-redis_url = 'redis://localhost:6379'
+redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
 
 # Create a connection to Redis
 conn = redis.from_url(redis_url)
